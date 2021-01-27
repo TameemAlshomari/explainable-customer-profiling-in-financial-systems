5eaf3049893e9979eb7ce200b5e8ac84ec369c5b
Sarmad Shaikh <sarmad.shaikh@cloudera.com>
2021-01-27 15:43:06 +0100

    Add run.sh to run the code

---
+++ faust/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Please make sure that Apache Kafka server is running before you run this script
+
+# install virtual environment package
+python -m pip install virtualenv
+
+# install virtual environment
+python -m virtualenv venv
+
+# make virtual environment executable
+chmod +x venv/bin/activate
+
+# enable virtual environment
+./venv/bin/activate
+
+# install requirements in virtual env
+pip install -r requirements.txt
+
+# run processing pipelines
+echo "Running invoice stream processing pipeline"
+faust -A invoice_history_processing worker -l info
+
+# start kafka-producer for invoices
+echo "Running kafka producer for invoices"
+python kafka_producer.py invoices invoice_history.csv &
